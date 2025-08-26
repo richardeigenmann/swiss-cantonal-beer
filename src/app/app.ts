a064bd15fc6e69fbf72dbdc9266ca9a4648e31ce
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-08-26 23:13:28 +0200

    Basic Canton selection built

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { CantonService, Canton } from './canton-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
+
+
 export class App {
   protected readonly title = signal('swiss-cantonal-beer');
+  private cantonService = inject(CantonService);
+  cantons = this.cantonService.cantons;
+
+  selectedCanton: Canton | null | undefined = null;;
+
+  onSelectionChange(selectedCanton: string) {
+    this.selectedCanton = this.cantonService.getCantonByCode(selectedCanton);
+  }
+
 }
